aa65c3995c4cccd86922302697f55856813ca7ec
kobzarmariia <mariia.kobzar@gmail.com>
2019-10-18 12:11:01 +0300

    [+] form validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './Button.css';
 
 const button = props => (
-	<button className={['Button', props.btnType].join(' ')} onClick={props.clicked}>
+	<button
+		className={['Button', props.btnType].join(' ')}
+		onClick={props.clicked}
+		disabled={props.disabled}
+	>
 		{props.children}
 	</button>
 );
